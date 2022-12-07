057e1edabfc534a75de752f6619b22043742c32d
Skoerdeman <magnus.adametz@student.htldornbirn.at>
2022-12-07 17:50:30 +0100

    simpe_calculator
    erstellen von simpe_calculator

---
+++ simple_calculator/src/components/SimpleButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="clickEvent"> {{ this.btnText }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        btnText: {
+            type: String,
+            required: false
+        }
+    },
+    emits: ["btnClicked"],
+    methods: {
+        clickEvent() {
+            this.$emit('btnClicked', this.btnText)
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    height: 80px;
+    background-color: rgb(255, 0, 0);
+    border: none;
+}
+</style>
---
+++ simple_calculator/src/components/TheCalculator.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="calc-flex">
+        <h1>Simple Calculator</h1>
+        <input ref="calcInput" class="input" v-model="this.calcText">
+        <div class="btns">
+            <simple-button @btnClicked="addOperation" :btnText="'+'"></simple-button>
+            <simple-button @btnClicked="addOperation" :btnText="'-'"></simple-button>
+            <simple-button @btnClicked="addOperation" :btnText="'*'"></simple-button> 
+            <simple-button @btnClicked="addOperation" :btnText="'/'"></simple-button>
+            <button class="calculateButton" @click="performOperation">=</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import SimpleButton from './SimpleButton.vue';
+export default {
+    data() {
+        return {
+            calcText: ""
+        }
+    },
+    components: { SimpleButton },
+    methods: {
+        addOperation(operationType) {
+            console.log(operationType)
+            this.calcText = this.calcText + " " + operationType + " ";
+            console.log(this.calcText)
+            this.$refs.calcInput.focus();
+        },
+        performOperation() {
+            try {
+                this.calcText = eval(this.calcText);
+            } catch (error) {
+                alert("Ungültiger Rechenstring")
+            }
+            this.$refs.calcInput.focus();
+        }
+    }
+}
+</script>
+
+<style>
+body {
+    background-color: rgb(255, 0, 149);
+    text-align: center;
+}
+
+.input {
+    font-size: 40px;
+    height: 50px;
+    width: 50%;
+}
+
+.calc-flex {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.btns {
+    justify-content: center;
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    width: 48.5%;
+    column-gap: 10px;
+    row-gap: 10px;
+    margin-top: 20px;
+}
+
+button {
+    font-size: 40px;
+}
+
+button:hover {
+    background-color: rgb(40, 0, 240);
+}
+
+.calculateButton {
+    grid-column-start: 1;
+    grid-column-end: 5;
+    height: 80px;
+    background-color: rgb(255, 0, 0);
+    border: none;
+}
+</style>
