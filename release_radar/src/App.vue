23b1baa49dcc7c64e6ae30e01acaa310b5c24a0d
masteroogway05 <piusstadler05@gmail.com>
2023-05-11 11:38:29 +0200

    mainpage
    mainpage start

--- release_radar/src/App.vue
+++ release_radar/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/RRLogo.png">
-  <HelloWorld msg="Release Radar"/>
+  <div class="max-w-7xl mx-auto"/>
+  <MainPage msg="Release Radar"/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainPage
   }
 }
 </script>
- 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
