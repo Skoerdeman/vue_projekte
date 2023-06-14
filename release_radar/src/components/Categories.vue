6d87e69865596ad879d64fb73494c04267d9dc58
Skoerdeman <magnus.adametz@student.htldornbirn.at>
2023-06-14 07:56:23 +0200

    neues Projekt
    release-radar hochgeladen

--- release_radar/src/components/Categories.vue
+++ release_radar/src/components/Categories.vue
@@ -5,7 +5,16 @@
         <img src="../assets/RRLogo.png">
         <li><a href="./MainPage.vue">Home</a></li>
         <li><a href="./Categories.vue">Categories</a></li>
-        <li><a href="#">Contact</a></li>
+        <div class="dropdown">
+  <button class="dropdown-btn">Kategorien</button>
+  <ul class="dropdown-content">
+    <li><a href="#">Option 1</a></li>
+    <li><a href="#">Option 2</a></li>
+    <li><a href="#">Option 3</a></li>
+    <!-- Weitere Optionen -->
+  </ul>
+</div>
+        <li><a href="#">Platform</a></li>
       </ul>
     </nav>
     <div class="container">
@@ -79,4 +88,43 @@ img{
   padding: 10px;
   margin: 10px;
 }
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-btn {
+  background-color: #f1f1f1;
+  color: black;
+  padding: 8px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {
+  background-color: #f1f1f1;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
 </style>
